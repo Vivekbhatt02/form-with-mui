b293fbd99202894635fe09a29db2f833a06f661b
Vivek <159415297+Vivekbhatt02@users.noreply.github.com>
2025-06-04 16:13:38 +0530

    Feature-1-Refactored the code

--- src/App.js
+++ src/App.js
@@ -1,62 +1,9 @@
-import React,{useState} from 'react';
-import {ThemeProvider} from '@emotion/react';
-import {customTheme} from './components/themes/customTheme';
-import {Box, Button, Container, FormControl, InputLabel, Input, Typography} from '@mui/material';
-
-const pagestyle = {
-  backgroundColor: 'rgb(239, 239, 239)',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-};
-
-const containerStyle = {
-  backgroundColor: 'white',
-  width: '20rem',
-  paddingTop: '4rem',
-  paddingBottom: '4rem',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  gap: '2rem',
-  borderRadius: '1rem',
-  boxShadow: [
-  "0.1875rem 0.1875rem 0.3125rem rgba(0, 0, 0, 0.2)",
-  " -0.1875rem -0.1875rem 0.3125rem rgba(0, 0, 0, 0.2)"
-  ].join(',')
-};
+import UserForm from "./components/UserForm.js";
 
 export default function App() {
-  const[name,setName] = useState('')
-  const[submittedName,setSubmittedName]= useState('')
-
-  function handleFormSubmit(event){
-    event.preventDefault()
-    setSubmittedName(name)
-    setName('')
-  }
   return (
-    <ThemeProvider theme={customTheme}>
-      <Box sx={pagestyle}>
-        <Container sx={containerStyle}>
-          <FormControl defaultValue="" required>
-            <InputLabel htmlFor="name-input">Name</InputLabel>
-            <Input id="name-input"
-              placeholder="Write your name here"
-              onChange={(event) => setName(event.target.value)}
-              value={name}
-            />
-          </FormControl>
-          <Button onClick={handleFormSubmit} variant='contained' sx={{width:'10rem'}}>
-            Submit
-          </Button>
-          <Typography sx={{ textAlign: 'center' }}>
-            Submitted Name:{submittedName}
-          </Typography>
-        </Container>
-      </Box>
-    </ThemeProvider>
-  );
+    <>
+      <UserForm/>
+    </>
+  )
 }
--- src/components/themes/customTheme.js
+++ src/components/themes/customTheme.js
@@ -2,9 +2,6 @@ import { createTheme } from "@mui/material";
 
 export const customTheme = createTheme({
     typography: {
-        fontFamily: [ 'cursive', 'Roboto',
-        'Raleway',
-        'Open Sans',
-        ].join(','),
+        fontFamily: 'cursive'
     }
 });
