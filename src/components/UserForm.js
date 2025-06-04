6d9a51adeb7912bfe1e9f5967340a9a56a9c9f1f
Vivek <159415297+Vivekbhatt02@users.noreply.github.com>
2025-06-04 16:25:26 +0530

    Feature-2 Added Gender and DOB fields

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,17 +1,23 @@
 import React,{useState} from 'react';
 
 import {ThemeProvider} from '@emotion/react';
-import {customTheme} from './themes/customTheme.js';
+import {customTheme} from './themes/customTheme';
 
 import {
-    Box,
-    Button,
-    Container,
-    FormControl,
-    InputLabel,
-    Input,
-    Typography
+  Box,
+  Button,
+  Container,
+  FormControl,
+  InputLabel,
+  Input,
+  Typography,
+  Select,
+  MenuItem
 } from '@mui/material';
+
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
 
 const pagestyle = {
   backgroundColor: 'rgb(239, 239, 239)',
@@ -29,7 +35,7 @@ const containerStyle = {
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
-  alignItems: 'center',
+  alignItems: 'flex-start',
   gap: '2rem',
   borderRadius: '1rem',
   boxShadow: [
@@ -38,20 +44,24 @@ const containerStyle = {
   ].join(',')
 };
 
-export default function UserForm() {
+export default function Userform() {
   const[name,setName] = useState('')
-  const[submittedName,setSubmittedName]= useState('')
+  const[gender,setGender] = useState('')
+  const [dob, setDOB] = useState(null);
+  const[submittedDetails,setSubmittedDetails]= useState({name:'',gender:'',dob: '',})
 
   function handleFormSubmit(event){
     event.preventDefault()
-    setSubmittedName(name)
+    setSubmittedDetails({name,gender,dob})
     setName('')
+    setGender('')
+    setDOB(null)
   }
   return (
     <ThemeProvider theme={customTheme}>
       <Box sx={pagestyle}>
         <Container sx={containerStyle}>
-          <FormControl defaultValue="">
+          <FormControl defaultValue="" required sx={{width:'100%'}}>
             <InputLabel htmlFor="name-input">Name</InputLabel>
             <Input id="name-input"
               placeholder="Write your name here"
@@ -59,12 +69,43 @@ export default function UserForm() {
               value={name}
             />
           </FormControl>
-          <Button onClick={handleFormSubmit} variant='contained' sx={{width:'10rem'}}>
+          <FormControl sx={{width:'100%'}}>
+            <InputLabel>Gender</InputLabel>
+            <Select 
+              label='Gender'
+              onChange={(event)=>setGender(event.target.value)}
+              value={gender}
+            >
+              <MenuItem value="Male" >Male</MenuItem>
+              <MenuItem value="Female" >Female</MenuItem>
+              <MenuItem value="Others" >Others</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl  sx={{width:'100%'}} >
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label={'DOB'}
+                  views={['year', 'month', 'day']}
+                  sx={{ minWidth: 250 }}
+                  onChange={(event)=>setDOB(event)}
+                  value={dob}
+                />
+            </LocalizationProvider>
+          </FormControl>
+          <Button onClick={handleFormSubmit} variant='contained' sx={{width:'100%'}}>
             Submit
           </Button>
-          <Typography sx={{ textAlign: 'center' }}>
-            Submitted Name:{submittedName}
-          </Typography>
+          <Box  sx={{ justifyContent: 'flex-start' }} >
+            <Typography>
+              Submitted Name:{submittedDetails.name}
+            </Typography>
+            <Typography>
+              Gender:{submittedDetails.gender}
+            </Typography>
+            <Typography>
+              Age:{submittedDetails.dob && submittedDetails.dob.format('DD-MM-YYYY')}
+            </Typography>
+          </Box>
         </Container>
       </Box>
     </ThemeProvider>
