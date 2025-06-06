903a0fee1ace7eaf1b479a3eace4c1bd9637fa79
Vivek <159415297+Vivekbhatt02@users.noreply.github.com>
2025-06-06 11:55:30 +0530

    Feature-1-Resolved comments

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,8 +1,4 @@
-import React,{useState} from 'react';
-
-import {ThemeProvider} from '@emotion/react';
-import {customTheme} from './themes/customTheme.js';
-
+import React, {useState} from 'react';
 import {
     Box,
     Button,
@@ -12,61 +8,66 @@ import {
     Input,
     Typography
 } from '@mui/material';
+import {ThemeProvider} from '@emotion/react';
 
-const pagestyle = {
-  backgroundColor: 'rgb(239, 239, 239)',
-  height: '100vh',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+import {customTheme} from './themes/customTheme';
+
+const pageStyle = {
+    backgroundColor: 'rgb(239, 239, 239)',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
 };
 
 const containerStyle = {
-  backgroundColor: 'white',
-  width: '20rem',
-  paddingTop: '4rem',
-  paddingBottom: '4rem',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  gap: '2rem',
-  borderRadius: '1rem',
-  boxShadow: [
-  "0.1875rem 0.1875rem 0.3125rem rgba(0, 0, 0, 0.2)",
-  " -0.1875rem -0.1875rem 0.3125rem rgba(0, 0, 0, 0.2)"
-  ].join(',')
+    backgroundColor: 'white',
+    width: '20rem',
+    paddingTop: '4rem',
+    paddingBottom: '4rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '2rem',
+    borderRadius: '1rem',
+    boxShadow: [
+        "0.1875rem 0.1875rem 0.3125rem rgba(0, 0, 0, 0.2)",
+        " -0.1875rem -0.1875rem 0.3125rem rgba(0, 0, 0, 0.2)"
+    ].join(',')
 };
 
 export default function UserForm() {
-  const[name,setName] = useState('')
-  const[submittedName,setSubmittedName]= useState('')
+    const [name, setName] = useState('')
+    const [submittedName, setSubmittedName] = useState('')
 
-  function handleFormSubmit(event){
-    event.preventDefault()
-    setSubmittedName(name)
-    setName('')
-  }
-  return (
-    <ThemeProvider theme={customTheme}>
-      <Box sx={pagestyle}>
-        <Container sx={containerStyle}>
-          <FormControl defaultValue="">
-            <InputLabel htmlFor="name-input">Name</InputLabel>
-            <Input id="name-input"
-              placeholder="Write your name here"
-              onChange={(event) => setName(event.target.value)}
-              value={name}
-            />
-          </FormControl>
-          <Button onClick={handleFormSubmit} variant='contained' sx={{width:'10rem'}}>
-            Submit
-          </Button>
-          <Typography sx={{ textAlign: 'center' }}>
-            Submitted Name:{submittedName}
-          </Typography>
-        </Container>
-      </Box>
-    </ThemeProvider>
-  );
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+        setSubmittedName(name)
+        setName('')
+    }
+
+    return (
+        <ThemeProvider theme={customTheme}>
+            <Box sx={pagestyle}>
+                <Container sx={containerStyle}>
+                    <FormControl defaultValue="">
+                        <InputLabel htmlFor="name-input">Name</InputLabel>
+                        <Input
+                            id="name-input"
+                            placeholder="Write your name here"
+                            onChange={(event) => setName(event.target.value)}
+                            value={name}
+                        />
+                    </FormControl>
+                    <Button onClick={handleFormSubmit} variant='contained' sx={{width: '10rem'}}>
+                        Submit
+                    </Button>
+                    <Typography sx={{textAlign: 'center'}}>
+                        Submitted Name:{submittedName}
+                    </Typography>
+                </Container>
+            </Box>
+        </ThemeProvider>
+    );
 }
