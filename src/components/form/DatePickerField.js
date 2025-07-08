9fd47fe16cd01537858b3e4f528dd50b1c411ca6
Vivek <159415297+Vivekbhatt02@users.noreply.github.com>
2025-07-08 12:48:08 +0530

    Feature-4 Formatted the code

--- src/components/form/DatePickerField.js
+++ src/components/form/DatePickerField.js
@@ -3,17 +3,17 @@ import moment from "moment";
 import {FormControl} from "@mui/material";
 import {DatePicker} from "@mui/x-date-pickers";
 
-export default function DatePickerField({ dob, setDOB }) {
+export default function DatePickerField({dob, setDOB}) {
 
     return (
-            <FormControl sx={{ width: '100%' }}>
-                <DatePicker
-                    label={'DOB'}
-                    views={['year', 'month', 'day']}
-                    sx={{ minWidth: 250 }}
-                    onChange={(event) => setDOB(event)}
-                    value={dob}
-                    maxDate={moment().startOf('day').subtract(1, 'day')}/>
-            </FormControl>
+        <FormControl sx={{width: '100%'}}>
+            <DatePicker
+                label={'DOB'}
+                views={['year', 'month', 'day']}
+                sx={{minWidth: 250}}
+                onChange={(event) => setDOB(event)}
+                value={dob}
+                maxDate={moment().startOf('day').subtract(1, 'day')}/>
+        </FormControl>
     )
 }
